f22fb8cbf625a8ad61ee5f826de43d6eefdeb975
=Justin Raymond <=jraymond@wesleyan.edu>
2013-09-06 19:49:40 -0400

    yippee

--- static/js/search.js
+++ static/js/search.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
   $(".event_search").chosen({no_results_text: "Nothing Happening :(",
-                             placeholder_text_single: "Search for events by name."});
+                             placeholder_text_multiple: "Search for events by name."});
 
   $(".category_search").chosen({no_results_text: "No categories found :(",
                            placeholder_text_multiple: "Filter by event category"});
