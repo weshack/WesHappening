0d4d6a9259448049b1b0f224596bb74fec06144f
Brian Gapinski <bgapinski@wesleyan.edu>
2013-09-06 16:25:32 +0000

    List of campus buildings

--- weshappening.py
+++ weshappening.py
@@ -17,6 +17,48 @@ class User(db.Model):
     def __repr__(self):
         return '<User %r>' % self.username
 
+
+class Event(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    location = db.relationship('Location', 
+        backref=db.backref('event', lazy='dynamic'))
+    time = db.Column(db.Time)
+    link = db.Column(db.String(100))
+    description = db.Column(db.Text)
+    category = db.Column(db.Integer)
+
+    def __init__(self, name, location, time, link, description, category):
+        self.name = name
+        self.location = location
+        self.time = time
+        self.link = link
+        self.description = description
+        self.category = category
+
+    def __repr__(self):
+        return '<Event %r>' % self.name
+
+
+class Location(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    short_name = db.Column(db.String(50))
+    lat = db.Column(db.Float)
+    lon = db.Column(db.Float)
+    addr = db.Column(db.String(100))
+
+    def __init__(self, name, short_name, lat, lon, addr):
+        self.name = name
+        self.short_name = short_name
+        self.lat = lat
+        self.lon = lon
+        self.addr = addr
+
+    def __repr__(self):
+        return '<Location %r>' % self.name
+
+
 @app.route('/')
 def index():
  return render_template("index.html")
