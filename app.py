b3b7529106cc0d294424fb424e361a9ad7426c9c
aaron <origamiboy13@gmail.com>
2013-09-06 13:30:02 -0400

    renamed the main function and added a sample map to the index.html, no mapping yet

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
-def app():
-  return "hello world!"
+@app.route("/")
+def hello():
+    return "Hello World!"
 
 if __name__ == "__main__":
-  app.run()
+    app.run()
