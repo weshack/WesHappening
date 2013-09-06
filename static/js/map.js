db07746a5f509723471e67462d1c9eb8e3ecb79e
aaron <origamiboy13@gmail.com>
2013-09-06 19:49:26 -0400

    minzoom again

--- static/js/map.js
+++ static/js/map.js
@@ -2,6 +2,7 @@ function initialize() {
   var mapOptions = {
     center: new google.maps.LatLng(41.5526833,-72.6612454),
     zoom: 15,
+    minzoom: 13;
     mapTypeId: google.maps.MapTypeId.ROADMAP
   };
   var map = new google.maps.Map(document.getElementById("map-canvas"),
