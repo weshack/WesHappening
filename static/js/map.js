8009771a685a8b86f3cccf6a4478194535d65578
Justin Raymond <justin.raymond.14@gmail.com>
2013-09-07 11:19:05 -0400

    consolidated search bar js with map js

--- static/js/map.js
+++ static/js/map.js
@@ -35,6 +35,11 @@ function initialize() {
   }
   
 
+  $("#event_search").chosen({no_results_text: "Nothing Happening :(", placeholder_text_multiple: "Search for events by name."});
+
+  $("#category_search").chosen({no_results_text: "No categories found :(", placeholder_text_multiple: "Filter by event category"});
+
+  $("#location_search").chosen({no_results_text: ":(", placeholder_text_multiple: "Filter event by location"});
 }
 google.maps.event.addDomListener(window, 'load', initialize);
 
