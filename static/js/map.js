926bcb80ff5a590cdf085d599bfe86984cc58d44
Justin Raymond <justin.raymond.14@gmail.com>
2013-09-07 11:42:46 -0400

    Fills location search with numbers

--- static/js/map.js
+++ static/js/map.js
@@ -1,4 +1,13 @@
 function initialize() {
+
+  /*
+   * Dynamically adds html options to a select menu with given id
+   */
+  function add_options(selector, options) {
+    for (var i=0;i<options.length;i++) {
+      $(selector).append("<option>"+options[i]+"<\\options>");    }
+  };
+
   var mapOptions = {
     center: new google.maps.LatLng(41.5526833,-72.6612454),
     zoom: 15,
@@ -34,12 +43,22 @@ function initialize() {
     marker.setMap(map);
   }
   
+  //adds options to the three search bars
+  var loc_names = [];
+  for (var i=0;i<locs.length;i++) {
+    loc_names.push(locs[i].name);
+  }
+  add_options('#location_search',loc_names);
 
+  /*
+   * Makes search bars pretty using the chosen framework
+   */
   $("#event_search").chosen({no_results_text: "Nothing Happening :(", placeholder_text_multiple: "Search for events by name."});
 
   $("#category_search").chosen({no_results_text: "No categories found :(", placeholder_text_multiple: "Filter by event category"});
 
   $("#location_search").chosen({no_results_text: ":(", placeholder_text_multiple: "Filter event by location"});
+
 }
 google.maps.event.addDomListener(window, 'load', initialize);
 
