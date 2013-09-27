80fdc4b845c9335edc0f30ff09fb6dc02fb364f7
aaron <origamiboy13@gmail.com>
2013-09-26 20:11:12 -0400

    working on parsing for wesevent case with no date

--- feed.py
+++ feed.py
@@ -49,25 +49,29 @@ for item in feed["items"]:
         name = name[9:]
     value = item["summary_detail"]["value"].split("<br />")
     value0 = str(no_unicode(value[0]))
-    date = re.match("\d\d/\d\d/\d\d\d\d", value0).group().split("/")
-    time = re.search("(TBA|\d\d:\d\d (a|p)m( - \d\d:\d\d (a|p)m)*)", value0).group().split(" ")
+    date = re.match("\d\d/\d\d/\d\d\d\d", value0)
+    if date:
+        date = date.group().split("/")
+        time = re.search("(TBA|\d\d:\d\d (a|p)m( - \d\d:\d\d (a|p)m)*)", value0).group().split(" ")
+    
+        if len(time) > 1:
+            t = time[0].split(":")
+            if (time[1] == "pm") and not (int(t[0]) == 12):
+                dt = datetime.datetime(int(date[2]), int(date[0]), int(date[1]), (int(t[0])+12)%24, int(t[1]))
+            elif (int(t[0]) == 12) and (t[1] == ("am")):
+                dt = datetime.datetime(int(date[2]), int(date[0]), int(date[1]), 0, int(t[1]))
+            else:
+                dt = datetime.datetime(int(date[2]), int(date[0]), int(date[1]), int(t[0]), int(t[1]))
+        else:
+            dt = datetime.datetime(int(date[2]), int(date[0]), int(date[1]))
+    else:
+        date = str(datetime.dateime.today())
     desc = value[0]
     loc = re.search("Location: .*", str(value[-1])).group().lstrip("Location: ")
     link = ""
     for v in value:
         if v.startswith("URL"):
             link = v.lstrip("URL: ")
-
-    if len(time) > 1:
-        t = time[0].split(":")
-        if (time[1] == "pm") and not (int(t[0]) == 12):
-            dt = datetime.datetime(int(date[2]), int(date[0]), int(date[1]), (int(t[0])+12)%24, int(t[1]))
-        elif (int(t[0]) == 12) and (t[1] == ("am")):
-            dt = datetime.datetime(int(date[2]), int(date[0]), int(date[1]), 0, int(t[1]))
-        else:
-            dt = datetime.datetime(int(date[2]), int(date[0]), int(date[1]), int(t[0]), int(t[1]))
-    else:
-        dt = datetime.datetime(int(date[2]), int(date[0]), int(date[1]))
 
     cat = ite % 4
     ite += 1
